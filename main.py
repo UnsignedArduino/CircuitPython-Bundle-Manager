073b4bc10c41d8e226f1be3c7637783019ade798
ckyiu <ckyiu@outlook.com>
2021-01-18 21:59:45 -0500

    Clear log on run

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import gui
 from bundle_tools.create_logger import create_logger
+from pathlib import Path
 import logging
 
 LEVEL = logging.DEBUG
+
+log_path = Path.cwd() / "log.log"
+log_path.write_text("")
 
 logger = create_logger(name=__name__, level=LEVEL)
 
